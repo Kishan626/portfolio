1107bb486b5eb7b94b53311756d0a9aefb927cf6
Kishan626 <146743870+Kishan626@users.noreply.github.com>
2025-09-10 00:16:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mobile Navbar toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+document.querySelectorAll(".nav-links a").forEach(link =>
+  link.addEventListener("click", () => navLinks.classList.remove("show"))
+);
